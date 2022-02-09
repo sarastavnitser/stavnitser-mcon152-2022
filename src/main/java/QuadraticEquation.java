f5a10a6d60b9b21ece29e66139ed41c3e82ddbd2
sarastavnitser <sstavnit@student.touro.edu>
2022-02-09 18:17:03 -0500

    practice commiting

--- src/main/java/QuadraticEquation.java
+++ src/main/java/QuadraticEquation.java
@@ -3,16 +3,16 @@ public class QuadraticEquation {
     private double b;
     private double c;
 
-    public QuadraticEquation(double a, double b, double c){
+    public QuadraticEquation(double a, double b, double c) {
         this.a = a;
         this.b = b;
         this.c = c;
     }
 
-    public double[] getX(){
+    public double[] getX() {
         double[] results = new double[2];
-        double num = Math.sqrt(b*b - 4*a*c);
-        double denominator = (2*a);
+        double num = Math.sqrt(b * b - 4 * a * c);
+        double denominator = (2 * a);
         results[0] = (-b + num) / denominator;
         results[1] = (-b - num) / denominator;
         return results;
